b9bd0dd9d6babe1026c0abf5b0cff81fb2251b7a
Oleksandr Galushka <bugblender@gmail.com>
2023-06-30 17:36:01 +0300

    Added timeline

---
+++ src/Dto.tsx
@@ -0,0 +1,36 @@
+export interface Artist {
+  id: string;
+  artist: string;
+  attendees: User[];
+  attending: boolean;
+  timeStart: string;
+  timeEnd: string;
+}
+
+export interface Stage {
+  stage: string;
+  artists: Artist[];
+}
+
+export interface Day {
+  date: string;
+  weekDay: string;
+  stages: Stage[];
+}
+
+export interface Schedule {
+  weekName: string;
+  weekNumber: number;
+  days: Day[];
+}
+
+export interface User {
+    name:string,
+    id:string
+}
+
+export interface ScheduleAPIResponse {
+  me: User;
+  owner: User;
+  schedule: Schedule[];
+}
